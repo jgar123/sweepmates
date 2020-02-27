b1a014ed895ec1160f847791ca3f646119be98dc
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-02-27 12:27:07 +0000

    call to API

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import Auth from '../lib/authMethods'
 
 const Home = () => {
+
+  const [groups, setGroups] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/user', {
+      headers: { Authorization: 'Bearer ' + Auth.getToken() }
+    })
+      .then(resp => {
+        setGroups(resp.data.groups)
+      })
+  }, [])
+
   return <section className="section">
-    
+    {groups.map((group, i) => {
+      return <div key={i}>
+        {group.name}
+      </div>
+    })}
   </section>
 }
 
